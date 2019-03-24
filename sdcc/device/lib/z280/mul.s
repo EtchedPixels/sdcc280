ddf104ff79024f1f4fc7c8c39fd15e8ae8fe5bfe
Alan Cox <alan@linux.intel.com>
2019-03-24 21:37:58 +0000

    z280: first cut at the support library code

---
+++ sdcc/device/lib/z280/mul.s
@@ -0,0 +1,52 @@
+;--------------------------------------------------------------------------
+;  mul.s
+;
+;  Copyright (C) 2018, Alan Cox
+;
+;  This library is free software; you can redistribute it and/or modify it
+;  under the terms of the GNU General Public License as published by the
+;  Free Software Foundation; either version 2, or (at your option) any
+;  later version.
+;
+;  This library is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License 
+;  along with this library; see the file COPYING. If not, write to the
+;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+;   MA 02110-1301, USA.
+;
+;  As a special exception, if you link this library with other files,
+;  some of which are compiled with SDCC, to produce an executable,
+;  this library does not by itself cause the resulting executable to
+;  be covered by the GNU General Public License. This exception does
+;  not however invalidate any other reasons why the executable file
+;   might be covered by the GNU General Public License.
+;--------------------------------------------------------------------------
+
+.area   _CODE
+
+.globl	__mulint
+
+	.z280
+
+;
+;	Should eventually end up in the code gen
+;
+__mulint:
+	ld	hl,2(sp)
+	ex	de,hl
+	ld	hl,4(sp)
+	multuw	hl,de
+	ret
+
+;
+;	Glue for 8bit multiplies until we sort the compiler out
+;	BC * DE into HL
+;
+__mul16::
+	ex	de,hl
+	multuw	hl,bc
+	ret
